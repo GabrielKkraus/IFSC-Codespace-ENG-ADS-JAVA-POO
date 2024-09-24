d0db4e7197378a3d752d706115adbe8404c6b8c7
GabrielKkraus <105125040+GabrielKkraus@users.noreply.github.com>
2024-09-24 19:51:25 +0000

    24_setembro-2

--- MeuLoopFor.java
+++ MeuLoopFor.java
@@ -1,6 +1,10 @@
+//clasecriada com o nome MeuLoopFor
 public class MeuLoopFor {
+   //dá inicio ao código, sempre é buscado o main
     public static void main(String[] args) {
-    for(int i=0; i<=10;i++){
+    //um for com i somando 1 até 10. 
+        for(int i=0; i<=10;i++){
+        //saida para o terminal com o valor de 1 mais a string elefante    
         System.out.println(i + " elefante");
     }
 }
